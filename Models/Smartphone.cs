9842be3ee4e14c76f69f820fa1833d3e86ef869c
Matheus Paiva <emanuelmatheus948@gmail.com>
2023-12-25 16:23:51 -0300

    desafio concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,25 +2,60 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        public Smartphone (string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            _modelo = modelo;
+            Imei = imei;
+            _memoria = memoria;
         }
+        public string Numero { get; set; }
+        private string _modelo;
+        private string _imei;
+        private int _memoria;
+
+        protected string Modelo => _modelo.ToUpper();
+        protected string Imei 
+        {
+            get {return _imei;}
+            set
+            {
+                try
+                {
+                    if(value.Length==15)
+                    {
+                        _imei = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException("O IMEI deve conter 15 dígitos");
+                    }
+                }
+                catch(ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    throw;
+                }
+            }
+        
+        }
+        protected int Memoria=> _memoria;
 
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
         }
-
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
         }
-
         public abstract void InstalarAplicativo(string nomeApp);
+
+        public void ExibirDataEHora()
+        {
+            DateTime data = DateTime.Now;
+            Console.WriteLine($"Hoje é {data.ToShortDateString()} e são {data.ToShortTimeString()}.");
+        }
+        
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "012345", modelo: "Modelo Y", imei: "111111111111111" , memoria: 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
+nokia.Numero = "022345";
+nokia.ExibirDataEHora();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "22445", modelo: "Modelo Z", imei: "222222222222222" , memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
+iphone.ExibirDataEHora();
